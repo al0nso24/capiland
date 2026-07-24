9ca694495bb4e4ea66549eb907f8b660d285efce
Alonso Zegarra Velásquez <zegarrav24@gmail.com>
2026-07-24 11:53:36 -0500

    Galería terminada.

--- src/Galeria.jsx
+++ src/Galeria.jsx
@@ -3,23 +3,23 @@ export default function Galeria(){
         <div className="img-galeria">
             <div className="container mt-5 mb-5">
                 <div className="row gy-4">
-                    <div className="col-12 col-md-4 col-lg-4 col-xl-4">
-                        <img src="/imagenes/capi-alimentacion.png" className="img-fluid" alt="capi-alimentacion"></img>
+                    <div className="col-12 col-md-6 col-lg-4 col-xl-4">
+                        <img src="/imagenes/capi-alimentacion.png" className="img-fluid shadow" alt="capi-alimentacion"></img>
                     </div>
-                    <div className="col-12 col-md-4 col-lg-4 col-xl-4">
-                        <img src="/imagenes/capi-habitat.png" className="img-fluid" alt="capi-habitat"></img>
+                    <div className="col-12 col-md-6 col-lg-4 col-xl-4">
+                        <img src="/imagenes/capi-habitat.png" className="img-fluid shadow" alt="capi-habitat"></img>
                     </div>
-                    <div className="col-12 col-md-4 col-lg-4 col-xl-4">
-                        <img src="/imagenes/capi-removebg.png" className="img-fluid" alt="capi-png"></img>
+                    <div className="col-12 col-md-6 col-lg-4 col-xl-4">
+                        <img src="/imagenes/capi-removebg.png" className="img-fluid shadow" alt="capi-png"></img>
                     </div>
                     <div className="col-12 col-md-12 col-lg-12 col-xl-12">
-                        <img src="/imagenes/capi-banner.png" className="img-fluid" alt="capi-banner"></img>
+                        <img src="/imagenes/capi-banner.png" className="img-fluid shadow" alt="capi-banner"></img>
                     </div>
-                    <div className="col-12 col-md-4 col-lg-4 col-xl-4">
-                        <img src="/imagenes/vida-social.png" alt="vida-social" className="img-fluid"></img>
+                    <div className="col-12 col-md-6 col-lg-4 col-xl-4">
+                        <img src="/imagenes/vida-social.png" alt="vida-social" className="img-fluid shadow"></img>
                     </div>
-                    <div className="col-12 col-md-4 col-lg-4 col-xl-4">
-                        <img src="/imagenes/capi-icon.png" className="img-fluid" alt="capi-icon"></img>
+                    <div className="col-12 col-md-6 col-lg-4 col-xl-4">
+                        <img src="/imagenes/capi-icon.png" className="img-fluid shadow" alt="capi-icon"></img>
                     </div>
                 </div>
             </div>
